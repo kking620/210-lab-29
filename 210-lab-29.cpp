68f85574bf0a55c147c1b123acaa62135828484e
Krispy King <kking620@gmail.com>
2025-11-05 15:10:57 -0800

    Sixth commit

--- 210-lab-29.cpp
+++ 210-lab-29.cpp
@@ -15,18 +15,31 @@
 using namespace std;
 
 //Defining function that will be used to simulate environmental changes over time
-void holiday_rush(map<string, tuple<int, int, int>>&);
-void weather_event(map<string, tuple<int, int, int>>&);
-void new_airport_opening(map<string, tuple<int, int, int>>&);
+void holiday_rush(map<string, airportDetails>&);
+void weather_event(map<string, airportDetails>&);
+void new_airport_opening(map<string, airportDetails>&);
 
-//Parameters for the map of the airports, as well as defining the array with the 
-using airportValues = int;
+//Defining the structure of the airports that we will be obtaining from our input files
+struct airportValues {
+    int nPeople;
+    int nDelays;
+    double avgReviews;
+};
+
+//Parameters for the map of the airports, as well as defining the array with the three different values we will be obtaining
 using airportDetails = array<list<airportValues>, 3>;
-map<string, airportDetails> airport;
+using airportMap = map<string, airportDetails>;
 
 //Defining the main function
 int main() {
+    //Initializing a map to store airport information, which is associated with an array for
+    //the number of people, the number of delays, and the average reviews
+    airportMap LAX;
+    airportMap JFK;
+    airportMap DCA;
 
+    //Open an external file
+    
 
 
 }
