bd1f05b5d4c50d269e8d7af9380d9a7e220f13a4
Krispy King <kking620@gmail.com>
2025-11-05 14:54:06 -0800

    Fifth commit

--- 210-lab-29.cpp
+++ 210-lab-29.cpp
@@ -1,8 +1,32 @@
+//Including the necessary headers for file handling, data structures, etc.
 #include <iostream>
 #include <iomanip>
+#include <fstream>
 #include <map>
 #include <list>
+#include <vector>
 #include <cstdlib>
 #include <ctime>
+#include <tuple>
+#include <string>
 #include <algorithm>
 #include <array>
+
+using namespace std;
+
+//Defining function that will be used to simulate environmental changes over time
+void holiday_rush(map<string, tuple<int, int, int>>&);
+void weather_event(map<string, tuple<int, int, int>>&);
+void new_airport_opening(map<string, tuple<int, int, int>>&);
+
+//Parameters for the map of the airports, as well as defining the array with the 
+using airportValues = int;
+using airportDetails = array<list<airportValues>, 3>;
+map<string, airportDetails> airport;
+
+//Defining the main function
+int main() {
+
+
+
+}
