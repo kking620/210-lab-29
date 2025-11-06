0ba4f0fcc67ff1101fe42693d272bf4398b3fa8a
Krispy King <kking620@gmail.com>
2025-11-05 16:03:20 -0800

    Seventh commit

--- 210-lab-29.cpp
+++ 210-lab-29.cpp
@@ -7,7 +7,6 @@
 #include <vector>
 #include <cstdlib>
 #include <ctime>
-#include <tuple>
 #include <string>
 #include <algorithm>
 #include <array>
@@ -15,31 +14,58 @@
 using namespace std;
 
 //Defining function that will be used to simulate environmental changes over time
-void holiday_rush(map<string, airportDetails>&);
-void weather_event(map<string, airportDetails>&);
-void new_airport_opening(map<string, airportDetails>&);
-
-//Defining the structure of the airports that we will be obtaining from our input files
-struct airportValues {
-    int nPeople;
-    int nDelays;
-    double avgReviews;
-};
-
-//Parameters for the map of the airports, as well as defining the array with the three different values we will be obtaining
-using airportDetails = array<list<airportValues>, 3>;
-using airportMap = map<string, airportDetails>;
+void holiday_rush(map<string, array<list<int>, 3>>&);
+void weather_event(map<string, array<list<int>, 3>>&);
+void new_airport_opening(map<string, array<list<int>, 3>>&);
 
 //Defining the main function
 int main() {
+    //Parameters for the map of the airports, as well as defining the array with the three different values we will be obtaining
+    using airportDetails = array<list<int>, 3>;
+
     //Initializing a map to store airport information, which is associated with an array for
     //the number of people, the number of delays, and the average reviews
-    airportMap LAX;
-    airportMap JFK;
-    airportMap DCA;
+    map<string, airportDetails> airports;
 
-    //Open an external file
+    //Open an external file to read the initial data about the airports to populate the map
+        //If the file does not open, print an error message and exit
+
+    //Read the data from the file and populate the map
+        //For each line, extract the name of the airport and the details of the airport
+        //Insert the approrpiate airport details into the map
+
+    //Close the file
+
+    //Begin a time-based simulation for the typical changes in the data for the airport
+
+        //for 26 time intervals
+            //Iterate through each airport in the map
+                //for each airport, simulate the changes that you will see
+                    //randomly decide if a computer error occurs, plane arrives early,
+                    //and a very low chance for a plane crash
+
+                    //depending on the random occurence, update the amount of people, delays, and reviews
+                        //For a computer error, increase nPeople, increase nDelays, and decrease avgReviews
+                        //For an early arrival, decrease nPeople, decrease nDealys, and increase avgReviews
+                        //For a plane crash, decrease nPeople, decrease nDelays, and keep avgReviews the same
+
+                    //print the changes in the values of the airport for the current time interval
+                        //e.g. "{nPeople} are now at the airport, there are {nDelays} reported, and "
     
 
+
+}
+
+
+//Defining the prototype functions that were initialized earlier in the code
+void holiday_rush(map<string, array<list<int>, 3>>&) {
+    
+}
+
+void weather_event(map<string, array<list<int>, 3>>&) {
+
+}
+
+void new_airport_opening(map<string, array<list<int>, 3>>&) {
 
 }
